8e8aaf7e9825c7c422a09a6c1c03f2eb5146f658
AyoubyAmazu <ayoubamazu@gmail.com>
2024-10-25 10:26:34 +0100

    live coding

---
+++ live_coding/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.app")
+@section("content")
+    <h1>welcom to ayoub site</h1>
+    <p>
+        this is the fisrt laravel app 
+    </p>
+@endsection    
--- live_coding/routes/web.php
+++ live_coding/routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/contact', [ContactController::class, 'create']);
-Route::post('/contact', [ContactController::class, 'store']);
+Route::get("/contact",[ContactController::class,"creat"]);
+Route::post("/contact",[ContactController::class,"store"]);
